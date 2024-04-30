7ab498533d010c4a61bd7237d05a83f8765d97c9
marco-battisti-04 <156814368+marco-battisti-04@users.noreply.github.com>
2024-04-30 15:20:57 +0000

    modificate routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,19 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'dashboard/:id',
+        component: DashboardComponent
+    }
+];
