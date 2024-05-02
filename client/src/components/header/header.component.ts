8ed25729de6b21dd338de5ccdc0a3ebee1b76693
marco-battisti-04 <156814368+marco-battisti-04@users.noreply.github.com>
2024-05-02 12:48:41 +0000

    modificato header della dashboard

--- client/src/components/header/header.component.ts
+++ client/src/components/header/header.component.ts
@@ -1,13 +1,31 @@
+import { DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
+  providers: [DatePipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
   @Input() ordernumber: string = "0000000";
+  @Input() otherid: string = "00000";
+
+  currentDate = new Date();
+  datePipe: DatePipe = new DatePipe('en-US');
+
+  constructor(private pipe: DatePipe) {
+
+  }
+
+  getCurrentdate() {
+    return this.datePipe.transform(this.currentDate, "dd/MM/yyyy");
+  }
+
+  getCurrentHour() {
+    return this.datePipe.transform(this.currentDate, "HH:mm:ss");
+  }
 }
