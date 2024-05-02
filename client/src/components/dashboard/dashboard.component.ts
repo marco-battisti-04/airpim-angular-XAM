d3dbe6fad53bd23b4ad54549aa24857e43414eba
marco-battisti-04 <156814368+marco-battisti-04@users.noreply.github.com>
2024-05-02 15:27:22 +0000

    aggiunta struttura, css della dashboard principale

--- client/src/components/dashboard/dashboard.component.ts
+++ client/src/components/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { KeyValueTextComponent } from '../key-value-text/key-value-text.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, KeyValueTextComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
