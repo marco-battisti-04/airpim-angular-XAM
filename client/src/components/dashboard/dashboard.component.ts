cb53e6603c7cd4b7c4191ee98266a2cca94cb53d
KemonoBAT4 <kemono.bat.4@gmail.com>
2024-05-03 12:17:52 +0100

    finita impostazione schermata principale

--- client/src/components/dashboard/dashboard.component.ts
+++ client/src/components/dashboard/dashboard.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { KeyValueTextComponent } from '../key-value-text/key-value-text.component';
+import { IMAGE_CONFIG } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { ImgSquareComponent } from '../img-square/img-square.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent, KeyValueTextComponent],
+  imports: [HeaderComponent, KeyValueTextComponent, ImgSquareComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent { }
