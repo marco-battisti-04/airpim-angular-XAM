5dff1fe8486a7736ba740b728088b23589f69a97
KemonoBAT4 <kemono.bat.4@gmail.com>
2024-05-06 12:03:18 +0200

    aggiunto handle dei parametri da url

--- client/src/components/dashboard/dashboard.component.ts
+++ client/src/components/dashboard/dashboard.component.ts
@@ -4,6 +4,8 @@ import { KeyValueTextComponent } from '../key-value-text/key-value-text.componen
 import { IMAGE_CONFIG } from '@angular/common';
 import { CommonModule } from '@angular/common';
 import { ImgSquareComponent } from '../img-square/img-square.component';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,4 +14,44 @@ import { ImgSquareComponent } from '../img-square/img-square.component';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent { }
+export class DashboardComponent {
+  ordernumber: string = "";
+  otherId: string = "";
+  left_header_text: string = "";
+  right_header_text: string = "";
+
+  constructor(
+    private http: HttpClient,
+    private route: ActivatedRoute
+  ) {
+
+    this.route.paramMap.subscribe(params => {
+      const mc = params.get('mc');
+      const orderId = params.get('id');
+      
+      try {
+        let mc2Number = Number(mc);
+        let OrderId2Number = Number(orderId);
+
+        this.getOrder(mc2Number, OrderId2Number);
+      }catch(error) {
+        console.error(error);
+      }
+    });
+  }
+
+  /**
+   * prende i dati degli ordini
+   * @param mc la macchina dove si trova l'ordine
+   * @param orderId l'id dell'ordine
+   */
+  async getOrder(mc: number, orderId: number) {
+
+    /*let response = await this.http.get(`localhost:50000/order/${mc}/${orderId}`).subscribe((data: any) => {
+      return data;
+    })*/
+
+    this.left_header_text = "test modificato dopo";
+    this.right_header_text = "test2 modificato dopo";
+  }
+}
