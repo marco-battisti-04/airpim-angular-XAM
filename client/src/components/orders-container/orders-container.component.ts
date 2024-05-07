01f9ff8c57f6a17fd381d43274fe6289592b89a3
KemonoBAT4 <kemono.bat.4@gmail.com>
2024-05-07 15:43:48 +0200

    aggiunto componente per gestione ordini

---
+++ client/src/components/orders-container/orders-container.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './orders-container.component.html',
+  styleUrl: './orders-container.component.css'
+})
+export class OrdersContainerComponent {
+  
+  @Input() id: number = 0;
+  @Input() name: string = "";
+  @Input() desc: string = "";
+
+
+  constructor() {
+    
+  }
+}
