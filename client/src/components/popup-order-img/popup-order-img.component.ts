e239d13590142fca4279ae597c04af1785aa6ec3
KemonoBAT4 <kemono.bat.4@gmail.com>
2024-05-08 12:02:26 +0200

    gestita immagine dal server

--- client/src/components/popup-order-img/popup-order-img.component.ts
+++ client/src/components/popup-order-img/popup-order-img.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-popup-order-img',
@@ -9,16 +10,30 @@ import { Component, EventEmitter, Input, Output, OnInit, ViewChild, ElementRef }
 })
 export class PopupOrderImgComponent implements OnInit {
 
-  @Input() img: string = "../../assets/images/img_test_1920x1080.jpg";
-
+  @Input() img: string = "";
   @Output() popup = new EventEmitter<boolean>();
 
-  constructor() { }
+  imageSrc!: SafeUrl;
+
+  constructor(private sanitizer: DomSanitizer) { 
+  }
   ngOnInit(): void {
   }
 
   closePopup() {
     this.popup.emit(false);
   }
-  
+
+  getImageSrc() {
+    console.log(this.img)
+    if(this.img === "") {
+      return "../../assets/images/backgrouond.jpg";
+    }else {
+      const imageBase64 = this.img;
+      const imageUrl = 'data:image/jpeg;base64,' + imageBase64;
+      this.imageSrc = this.sanitizer.bypassSecurityTrustUrl(imageUrl);
+      console.log(this.imageSrc)
+      return this.imageSrc;
+    }
+  }
 }
