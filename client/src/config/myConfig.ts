6b99966d8812741a32107bb75bcd8b06cb93b342
KemonoBAT4 <kemono.bat.4@gmail.com>
2024-05-29 10:29:36 +0200

    cambiato ip del server api

--- client/src/config/myConfig.ts
+++ client/src/config/myConfig.ts
@@ -1,5 +1,5 @@
 export class myConfig {
-    private SERVER_URL: string = "http://10.0.0.152:50000"
+    private SERVER_URL: string = "http://localhost:50000"
 
     getServerUrl(): string {
         return this.SERVER_URL;
